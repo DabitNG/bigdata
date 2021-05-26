9f904b04ab1306cecd89e2b169d3d6f7f2b53769
David Nuño <davidnunogarcia@gmail.com>
2021-05-26 19:45:26 +0200

    REFACTOR, ADD -d as second arg for detached mode, ADD stack name as first argument

--- launch.sh
+++ launch.sh
@@ -1,2 +1,26 @@
-cmd="docker-compose -f docker-compose.stack.yml -p bigdata_stack up --force-recreate --remove-orphans"
-eval $cmd
+
+# Log function
+log() {
+    echo "$1"
+    echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") [PREPARE] $1" >> out.txt
+}
+
+# Log and execute string argument as command
+logAndExe() {
+    log "$1"
+    eval $1
+}
+
+project=$1
+detached=$2
+if [ "$project" == "" ]; then
+    project="bigdata_stack"
+fi
+if [ "$detached" != "" ]; then
+    detached=" -d"
+fi
+log "Launching $project"
+logAndExe "docker-compose -f docker-compose.stack.yml -p $project up --force-recreate --remove-orphans$detached"
+
+read -n 1 -s -r -p "Press any key to close"
+exit 0
