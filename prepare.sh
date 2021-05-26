56ae758edafbd560a86f7cde7fe6ba520a2ba8de
David Nuño <davidnunogarcia@gmail.com>
2021-05-26 20:09:36 +0200

    UPD some improves

--- prepare.sh
+++ prepare.sh
@@ -1,25 +1,23 @@
 # This script generates docker compose stack file with passed as arguments as working services and launches them
 # Common vars
-backup="./backup"
-dest="./.env"
-dcs_bckp="./docker-compose.stack.yml"
+backup="backup"
+dest=".env"
+dcs_bckp="docker-compose.stack.yml"
 
 # Log function
 log() {
-    echo "$1"
-    echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") [PREPARE] $1" >> out.txt
+    echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") [PREPARE] $1"
 }
 
 # Log and execute string argument as command
 logAndExe() {
     log "$1"
-    eval $1
+    eval "$1"
 }
 
 # Move previous .env file if exists
 logAndExe "mkdir backup"
 logAndExe "mv $dest $backup/$dest.backup"
-logAndExe "mv $dc_bckp $backup/$dc_bckp.backup"
 logAndExe "mv $dcs_bckp $backup/$dcs_bckp.backup"
 
 
@@ -35,7 +33,7 @@ do
     log "Service: $service"
     if [[ $services == *$service* ]]; then
         logAndExe "cat ./$service/.env >> $dest"
-        cmd="$cmd -f ${var,,}/docker-compose.yml"
+        cmd="$cmd -f $service/docker-compose.yml"
     else
         log "Unknown service: $service, skipping"
     fi
@@ -43,5 +41,4 @@ done
 
 # create docker compose stack file
 logAndExe "$cmd config >| docker-compose.stack.yml"
-
-read -n 1 -s -r -p "Press any key to close"
+read -n 1 -s -r -p "$(date +"%Y-%m-%d %H:%M:%S,%3N") [PREPARE] OK. Press any key to close."
