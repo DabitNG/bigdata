4a4ada7f5d913e2ad212df4d8f33722e4152879e
David Nuño <davidnunogarcia@gmail.com>
2021-05-28 16:52:08 +0200

    UPD spark compose block

--- prepare.sh
+++ prepare.sh
@@ -21,7 +21,7 @@ defineSparkWorkers(){
     i=0
     while [ $i -lt $1 ]
     do
-        log "Worker #$i"
+        log "Generating Worker #$i..."
         logAndExe "cat ./spark/nginx/spark.conf.template >| ./spark/nginx/spark-wk$i.conf"
         logAndExe "sed -i -e 's/worker/wk$i/g' ./spark/nginx/spark-wk$i.conf"
         logAndExe "cat ./spark/worker.yml.template >| ./spark/docker-compose-wk$i.yml"
@@ -49,8 +49,6 @@ do
 
     if [[ "$service" == *"spark"* ]]; then          # Spark service request
         logAndExe "cat ./spark/.env >> $dest"
-        cmd="$cmd -f ./spark/docker-compose.yml"    # Spark base added to command
-
         workers=1                                   # default worker nodes
         
         if [[ "$service" == *"="* ]]; then          # Get worker nodes
@@ -62,10 +60,26 @@ do
         fi
 
         defineSparkWorkers "$workers"               # Generate workers conf
+
+        log "Generating spark stack..."
+
+        sparkstack="docker-compose -f ./spark/docker-compose.yml"
+
         i=0
-        while [ $i -lt $workers ]                   # Foreach worker add compose file to cmd
+        while [ $i -lt $workers ]                   # Foreach worker add compose file to sparkstack
         do
-            cmd="$cmd -f ./spark/docker-compose-wk$i.yml"
+            sparkstack="$sparkstack -f ./spark/docker-compose-wk$i.yml"
+            i=$[$i+1]
+        done
+        echo "$sparkstack"
+        logAndExe "$sparkstack config >| ./spark/docker-compose.stack.yml"
+        
+        cmd="$cmd -f ./spark/docker-compose.stack.yml"
+        echo "$cmd"
+        i=0
+        while [ $i -lt $workers ]                   # Foreach worker add compose file to sparkstack
+        do
+            logAndExe "rm ./spark/docker-compose-wk$i.yml"
             i=$[$i+1]
         done
     elif [[ $service == *$services* ]]; then
